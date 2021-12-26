fa801ee0ede486189772c18c53c342d8e224092e
Ya-dola <Ya-dola@users.noreply.github.com>
2021-12-26 23:29:24 +0530

    Added Props to LaneSummoner

--- lol_s_t/src/Home.js
+++ lol_s_t/src/Home.js
@@ -2,19 +2,23 @@ import React, {Fragment} from 'react';
 import {Main} from "grommet";
 import HeaderBar from "./HeaderBar";
 import LaneSummoner from "./LaneSummoner";
+import TopIcon from "./pos_icons/icon-position-top.png"
+import JgIcon from "./pos_icons/icon-position-jungle.png"
+import MidIcon from "./pos_icons/icon-position-middle.png"
+import BotIcon from "./pos_icons/icon-position-bottom.png"
+import SuppIcon from "./pos_icons/icon-position-utility.png"
 
 function Home() {
 
     return (
         <Fragment>
-            {/*<h1>Ello</h1>*/}
             <Main fill="vertical" direction="column" justify="start" align="center">
                 <HeaderBar/>
-                <LaneSummoner/>
-                {/*<LaneSummoner/>*/}
-                {/*<LaneSummoner/>*/}
-                {/*<LaneSummoner/>*/}
-                {/*<LaneSummoner/>*/}
+                <LaneSummoner lane={"Top"} laneIcon={TopIcon}/>
+                {/*<LaneSummoner lane={"Jungle"} laneIcon={JgIcon}/>*/}
+                {/*<LaneSummoner lane={"Mid"} laneIcon={MidIcon}/>*/}
+                {/*<LaneSummoner lane={"Bot"} laneIcon={BotIcon}/>*/}
+                {/*<LaneSummoner lane={"Support"} laneIcon={SuppIcon}/>*/}
             </Main>
         </Fragment>
     );
--- lol_s_t/src/LaneSummoner.js
+++ lol_s_t/src/LaneSummoner.js
@@ -1,18 +1,17 @@
 import React, {Fragment} from "react";
 import {Box, Image, Text} from "grommet";
 import SummFlash from "./SummFlash";
-import TopIcon from "./pos_icons/icon-position-top.png"
 
-function LaneSummoner() {
+function LaneSummoner(props) {
     return (
         <Fragment>
             <Box align="center" justify="center" direction="row" pad="small" margin="small" overflow="visible"
                  responsive width="large">
                 <Box align="center" justify="center" width="medium" height="small" direction="column" gap="medium"
                      responsive wrap overflow="auto">
-                    <Image src={TopIcon} fit="contain" width={"100px"}/>
+                    <Image src={props.laneIcon} fit="contain" width={"100px"}/>
                     <Text textAlign="start" size="large">
-                        Top
+                        {props.lane}
                     </Text>
                 </Box>
                 <SummFlash/>
