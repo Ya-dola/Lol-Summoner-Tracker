20f442378e4c1acafb8c5f9b3676fa3095fcf2ea
Ya-dola <Ya-dola@users.noreply.github.com>
2021-12-26 17:14:41 +0530

    Got Bare App Working

--- lol_s_t/src/App.js
+++ lol_s_t/src/App.js
@@ -1,27 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Routes, Route, Link, BrowserRouter} from "react-router-dom";
+import Home from "./Home";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1>ELLOOO</h1>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
---
+++ lol_s_t/src/Home.js
@@ -0,0 +1,12 @@
+import React, {Fragment} from 'react';
+
+function Home() {
+
+    return (
+        <Fragment>
+            <h1>Ello</h1>
+        </Fragment>
+    );
+}
+
+export default Home;
